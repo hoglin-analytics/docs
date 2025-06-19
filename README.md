978ddfb5498ba120011eb42a52ad1cf92fbf9319
flowergardn <me@astrid.sh>
2025-06-19 14:42:21 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hoglin Documentation
+
+The open source documentation for [hoglin.gg](https://hoglin.gg), powered by [Starlight 🌟](https://starlight.astro.build/)
---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+  		plugins: [starlightThemeFlexoki({
+   		  accentColor: "orange",
+  		})],
+      customCss: ["/src/styles/custom.css"],
+			title: 'Hoglin',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/hoglin-analytics/docs' }],
+			sidebar: [
+  			{ label: 'Introduction', link: '/introduction' },
+        { label: 'Getting Started', link: '/getting-started' },
+        { label: 'Pricing', link: '/pricing' },
+        {
+            label: 'Guides',
+            items: [
+              { label: 'Create a server', link: '/guides/create-server' },
+              { label: 'Track player events', link: '/guides/player-events' },
+              { label: 'Explore your data', link: '/guides/explore' },
+            ],
+        },
+        {
+            label: 'Reference',
+            items: [
+              { label: 'SDK Reference', link: '/reference/sdk' },
+            ],
+        },
+			],
+		}),
+	],
+});
