653d514cc714ef9d9b014e1ea9bd0245dfca368d
Aroze <admin@aroze.me>
2025-08-08 00:54:05 +0100

    feat: new docs structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,22 +15,48 @@ export default defineConfig({
 			title: 'Hoglin',
 			sidebar: [
   			{ label: 'Introduction', link: '/introduction' },
-        { label: 'Getting Started', link: '/getting-started' },
         { label: 'Pricing', link: '/pricing' },
         { label: 'Contribute', link: '/contribute' },
         {
             label: 'Guides',
             items: [
-              { label: 'Create a server', link: '/guides/create-server' },
-              { label: 'Track player events', link: '/guides/player-events' },
-              { label: 'Explore your data', link: '/guides/explore' },
-              { label: 'Learn about Experiments', link: '/guides/experiments' },
+              { label: 'Create a Hoglin server', link: '/guides/create-hoglin-server' },
+              { label: 'Exploring your data', link: '/guides/exploring-your-data' },
+              { label: 'Visualisations', link: '/guides/visualisations' },
+              { label: 'Experiments', link: '/guides/experiments' },
             ],
         },
         {
-            label: 'Reference',
+            label: 'Public plugins',
             items: [
-              { label: 'SDK Reference', link: '/reference/sdk' },
+              {
+                label: 'PlayerTracker Plugin',
+                items: [
+                  { label: 'What is PlayerTracker', link: '/public-plugins/playertracker/what-is-playertracker' },
+                  { label: 'Installation', link: '/public-plugins/playertracker/installation' },
+                  { label: 'Configuration', link: '/public-plugins/playertracker/configuration' },
+                ],
+              },
+            ],
+        },
+        {
+            label: 'Developer SDK',
+            items: [
+              { label: 'Installation', link: '/developer-sdk/installation' },
+              { label: 'Configuring your instance', link: '/developer-sdk/configuring-your-instance' },
+              {
+                label: 'Tracking analytics',
+                items: [
+                  { label: 'Introduction to tracking', link: '/developer-sdk/tracking-analytics/introduction-to-tracking' },
+                  { label: 'Basic tracking', link: '/developer-sdk/tracking-analytics/basic-tracking' },
+                  { label: 'Analytic and NamedAnalytic', link: '/developer-sdk/tracking-analytics/analytic-and-namedanalytic' },
+                  { label: 'Overriding timestamps', link: '/developer-sdk/tracking-analytics/overriding-timestamps' },
+                  { label: 'Custom types and serializers', link: '/developer-sdk/tracking-analytics/custom-types-and-serializers' },
+                ],
+              },
+              { label: 'Evaluating experiments', link: '/developer-sdk/evaluating-experiments' },
+              { label: 'Safely shutting down', link: '/developer-sdk/safely-shutting-down' },
+              { label: 'Example plugins', link: '/developer-sdk/example-plugins' },
             ],
         },
 			],
