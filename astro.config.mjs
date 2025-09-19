d1c5a0e4c5aa7a6b75349e78dd1c70c1f80c0308
Aroze <admin@aroze.me>
2025-09-19 16:19:46 +0100

    Rework docs (#1)
    * feat: new docs structure
    
    * feat: installation page
    
    * feat: creating your instance page, add icons
    
    * feat: introduction to tracking page
    
    * feat: tracking with maps page, make text styling consistent
    
    * feat: add real image for tracked event example
    
    * fix: typo in tracking with maps documentation
    
    * feat: more sensical tracked event example
    
    * feat: update builder defaults and document #requeueFailedFlushes
    
    * feat: analytic and namedanalytic page
    
    * feat: custom types and serializers
    
    * fix: add missing imports
    
    * feat: better custom gson instance example
    
    * refactor: GSON -> Gson
    
    * feat: overriding timestamps
    
    * feat: safely shutting down
    
    * feat: evaluating experiments
    
    * feat: bump sdk version to 1.0.10 in maven usage
    
    * fix: casing in gson example
    
    * fix: clarify wording for onDisable example
    
    * feat: document playertracker plugin
    
    * feat: document Hoglin#evaluateExperimentRaw
    
    * fix: various grammar fixes/clarity improvements
    
    * fix: made some further changes based on suggestions in PR
    
    ---------
    
    Co-authored-by: ansxor <darienreese@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,22 +15,47 @@ export default defineConfig({
 			title: 'Hoglin',
 			sidebar: [
   			{ label: 'Introduction', link: '/introduction' },
-        { label: 'Getting Started', link: '/getting-started' },
         { label: 'Pricing', link: '/pricing' },
         { label: 'Contribute', link: '/contribute' },
         {
             label: 'Guides',
             items: [
-              { label: 'Create a server', link: '/guides/create-server' },
-              { label: 'Track player events', link: '/guides/player-events' },
-              { label: 'Explore your data', link: '/guides/explore' },
-              { label: 'Learn about Experiments', link: '/guides/experiments' },
+              { label: 'Create a Hoglin Server', link: '/guides/create-hoglin-server' },
+              { label: 'Exploring Your Data', link: '/guides/exploring-your-data' },
+              { label: 'Visualisations', link: '/guides/visualisations' },
+              { label: 'Experiments', link: '/guides/experiments' },
             ],
         },
         {
-            label: 'Reference',
+            label: 'Public Plugins',
             items: [
-              { label: 'SDK Reference', link: '/reference/sdk' },
+              {
+                label: 'PlayerTracker Plugin',
+                items: [
+                  { label: 'What is PlayerTracker', link: '/public-plugins/playertracker/what-is-playertracker' },
+                  { label: 'Installation', link: '/public-plugins/playertracker/installation' },
+                  { label: 'Configuration', link: '/public-plugins/playertracker/configuration' },
+                ],
+              },
+            ],
+        },
+        {
+            label: 'Developer SDK',
+            items: [
+              { label: 'Installation', link: '/developer-sdk/installation' },
+              { label: 'Creating Your Instance', link: '/developer-sdk/creating-your-instance' },
+              {
+                label: 'Tracking Analytics',
+                items: [
+                  { label: 'Introduction To tracking', link: '/developer-sdk/tracking-analytics/introduction-to-tracking' },
+                  { label: 'Tracking with Maps', link: '/developer-sdk/tracking-analytics/tracking-with-maps' },
+                  { label: 'Analytic and NamedAnalytic', link: '/developer-sdk/tracking-analytics/analytic-and-namedanalytic' },
+                  { label: 'Overriding Timestamps', link: '/developer-sdk/tracking-analytics/overriding-timestamps' },
+                  { label: 'Custom Types and Serializers', link: '/developer-sdk/tracking-analytics/custom-types-and-serializers' },
+                ],
+              },
+              { label: 'Evaluating Experiments', link: '/developer-sdk/evaluating-experiments' },
+              { label: 'Safely Shutting Down', link: '/developer-sdk/safely-shutting-down' },
             ],
         },
 			],
